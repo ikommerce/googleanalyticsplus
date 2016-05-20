63e097fbff835e0470546f632354e46fb291db84
Marco Andreini <marco.andreini@gmail.com>
2016-05-20 12:02:00 +0200

    Implemented ga events for add user to newsletter and add item to cart.

--- app/code/community/Fooman/GoogleAnalyticsPlus/Model/Observer.php
+++ app/code/community/Fooman/GoogleAnalyticsPlus/Model/Observer.php
@@ -17,40 +17,48 @@ class Fooman_GoogleAnalyticsPlus_Model_Observer
     {
         Mage::register('googleanalyticsplus_order_ids', $observer->getEvent()->getOrderIds(), true);
     }
-   
-   public function checkoutCartAdd(){
+
+    public function checkoutCartAdd($observer)
+    {
 	   	$product = Mage::getModel('catalog/product')
-	   	->load(Mage::app()->getRequest()->getParam('product', 0));
-	   	
+	   	 ->load(Mage::app()->getRequest()->getParam('product', 0));
+
 	   	if (!$product->getId()) {
 	   		return;
 	   	}
-	   	
+
 	   	$category_name = '';
 	   	$categories = $product->getCategoryIds();
-	   	
-	   	if(count($categories) ){
-	   		$count=0;
-	   		foreach($categories as $k => $firstCategoryId):
-	   		if($count>0){$category_name .= ' > ';}
-	   		$category_name .= Mage::getModel('catalog/category')->load($firstCategoryId)->getName();
-	   		$count++;
+
+	   	if (count($categories)) {
+			$count = 0;
+	   		foreach ($categories as $k => $firstCategoryId):
+	   			if($count>0) {
+	   				$category_name .= ' > ';
+	   			}
+	   			$category_name .= Mage::getModel('catalog/category')->load($firstCategoryId)->getName();
+	   			$count++;
 	   		endforeach;
 	   	}
 	   	$price = $product->getData("special_price");
-	   	if(!$price){$price = $product->getPrice();}
+	   	if (!$price) {
+	   		$price = $product->getPrice();
+	   	}
 	   	$price = number_format($price,2,".","");
-	   	
+
 	   	Mage::getModel('core/session')->setProductToShoppingCart(
-	   			new Varien_Object(array(
-	   					'id' => $product->getId(),
-	   					'qty' => Mage::app()->getRequest()->getParam('qty', 1),
-	   					'name' => $product->getName(),
-	   					'price' => $price,
-	   					'category_name' => $category_name,
-	   			))
-	   	);
+	   		new Varien_Object(array(
+	   			'id' => $product->getId(),
+	   			'qty' => Mage::app()->getRequest()->getParam('qty', 1),
+	   			'name' => $product->getName(),
+	   			'price' => $price,
+	   			'category_name' => $category_name,
+	   		))
+	   );
    	}
-    
-    
+
+   	public function addToNewsletter($observer)
+   	{
+		Mage::getModel('core/session')->setAddedToNewsletter(true);
+   	}
 }
